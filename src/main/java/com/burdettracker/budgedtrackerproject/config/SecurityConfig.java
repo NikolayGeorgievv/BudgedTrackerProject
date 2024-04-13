601c0b46066dd6103692f45da16231be0ef56f5d
NikolayGeorgievv <skullronx@gmail.com>
2024-04-13 15:04:43 +0300

    Fixed an error with loading images.

--- src/main/java/com/burdettracker/budgedtrackerproject/config/SecurityConfig.java
+++ src/main/java/com/burdettracker/budgedtrackerproject/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,32 +16,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(
-                authorizeRequests -> authorizeRequests
-                        // Allow all static resources to be available.
-                        .requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations())).permitAll()
-                        .requestMatchers("src/main/resources/static/pics").permitAll()
-                        // Allow anyone to see the home page, the registration page and the login form.
-                        .requestMatchers("/homePage", "/users/login", "/users/register", "/").permitAll()
-                        .anyRequest().authenticated()
-        ).formLogin(
-                formLogin -> {
-                    formLogin
-                            // redirect here when we access something which is not allowed.
-                            // also this is the page where we perform login.
-                            .loginPage("/homePage")
-                            // The names of the input fields (in our case in login.html)
-                            .usernameParameter("email")
-                            .passwordParameter("password")
-                            .defaultSuccessUrl("/index")
-                            .failureForwardUrl("/homePage");
-                }
-        ).logout(
-                logout -> {
-                    logout
-                            .logoutUrl("/logout")
-                            .logoutSuccessUrl("/homePage")
-                            .invalidateHttpSession(true);
-                }
+                        authorizeRequests -> authorizeRequests
+                                // Allow all static resources to be available.
+                                .requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations())).permitAll()
+                                .requestMatchers("/images/**").permitAll()
+//                                // Allow anyone to see the home page, the registration page and the login form.
+                                .requestMatchers("/homePage", "/users/login", "/users/register", "/").permitAll()
+                                .anyRequest().authenticated()
+                ).formLogin(
+                        formLogin -> {
+                            formLogin
+                                    // redirect here when we access something which is not allowed.
+                                    // also this is the page where we perform login.
+                                    .loginPage("/homePage")
+                                    // The names of the input fields (in our case in login.html)
+                                    .usernameParameter("email")
+                                    .passwordParameter("password")
+                                    .defaultSuccessUrl("/index")
+                                    .failureForwardUrl("/homePage");
+                        }
+                ).logout(
+                        logout -> {
+                            logout
+                                    .logoutUrl("/logout")
+                                    .logoutSuccessUrl("/homePage")
+                                    .invalidateHttpSession(true);
+                        }
 //        ).rememberMe(
 //                rememberMe ->
 //                        rememberMe
@@ -49,7 +50,10 @@ public class SecurityConfig {
 //                                .rememberMeCookieName("rememberme")
 //        ).oauth2Login(
 //                oauth -> oauth.successHandler(oAuthSuccessHandler)
-        ).build();
+                ).csrf(AbstractHttpConfigurer::disable)
+                .build();
+
+
     }
 
 }
--- src/main/java/com/burdettracker/budgedtrackerproject/config/WebConfig.java
+++ src/main/java/com/burdettracker/budgedtrackerproject/config/WebConfig.java
@@ -8,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/")
-                .addResourceLocations("classpath:/fragments/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
