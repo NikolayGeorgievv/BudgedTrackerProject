370e2178e9f30a444c0fd35260a09bf85e3f4d1a
NikolayGeorgievv <skullronx@gmail.com>
2024-04-19 10:28:16 +0300

    Implement user detail dto for mainPage controller.

---
+++ src/main/java/com/burdettracker/budgedtrackerproject/model/dto/UserExpensesDetailsDTO.java
@@ -0,0 +1,122 @@
+package com.burdettracker.budgedtrackerproject.model.dto;
+
+import com.burdettracker.budgedtrackerproject.model.entity.*;
+import com.burdettracker.budgedtrackerproject.model.entity.enums.MembershipType;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+public class UserExpensesDetailsDTO {
+
+    private UUID id;
+    private MembershipType membershipType;
+    private String phoneNumber;
+    private BigDecimal totalBalance;
+    private List<Account> accounts;
+    private List<UserDocument> userDocuments;
+    private List<Goal> goals;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<Expense> expenses;
+    private Address address;
+
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public MembershipType getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(MembershipType membershipType) {
+        this.membershipType = membershipType;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BigDecimal getTotalBalance() {
+        return totalBalance;
+    }
+
+    public void setTotalBalance(BigDecimal totalBalance) {
+        this.totalBalance = totalBalance;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public List<UserDocument> getUserDocuments() {
+        return userDocuments;
+    }
+
+    public void setUserDocuments(List<UserDocument> userDocuments) {
+        this.userDocuments = userDocuments;
+    }
+
+    public List<Goal> getGoals() {
+        return goals;
+    }
+
+    public void setGoals(List<Goal> goals) {
+        this.goals = goals;
+    }
+}
