42efb693718e4e333d46322e93d89de3cb89547c
NikolayGeorgievv <skullronx@gmail.com>
2024-05-14 21:17:10 +0300

    More Goals implementation. Currently there is an error with displaying all the goals.(chunk encoding)

--- src/main/java/com/burdettracker/budgedtrackerproject/web/GoalsController.java
+++ src/main/java/com/burdettracker/budgedtrackerproject/web/GoalsController.java
@@ -1,15 +1,11 @@
 package com.burdettracker.budgedtrackerproject.web;
 
 import com.burdettracker.budgedtrackerproject.model.dto.expense.ExpenseDTO;
-import com.burdettracker.budgedtrackerproject.model.dto.goal.AllGoalsInfoDTO;
 import com.burdettracker.budgedtrackerproject.model.dto.goal.GoalDTO;
 import com.burdettracker.budgedtrackerproject.service.account.AccountService;
 import com.burdettracker.budgedtrackerproject.service.expense.ExpenseService;
 import com.burdettracker.budgedtrackerproject.service.goals.GoalsService;
 import com.burdettracker.budgedtrackerproject.service.user.UserService;
-import com.google.gson.Gson;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -24,31 +20,18 @@ public class GoalsController extends BaseController{
 
     private final GoalsService goalsService;
     private final UserService userService;
-    private final Gson gson;
-    public GoalsController(List<ExpenseDTO> expenses, UserService userService, ExpenseService expenseService, AccountService accountService, GoalsService goalsService, UserService userService1, Gson gson) {
+    public GoalsController(List<ExpenseDTO> expenses, UserService userService, ExpenseService expenseService, AccountService accountService, GoalsService goalsService) {
         super(expenses, userService, expenseService, accountService, goalsService);
         this.goalsService = goalsService;
-        this.userService = userService1;
-        this.gson = gson;
+        this.userService = userService;
     }
 
-//    @GetMapping("/allGoalsPage")
-//    public String allGoalsPage(){
-//
-//        return "allGoalsPage";
-//    }
+    @GetMapping("/allGoalsPage")
+    public String allGoalsPage(){
 
-//    @GetMapping("/allGoalsPage")
-//    public ResponseEntity<String> responseEntity(){
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        String currentUserName = authentication.getName();
-//        AllGoalsInfoDTO allGoals = this.goalsService.getAllGoals(currentUserName);
-//        Long length = goalLength(allGoals);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add(HttpHeaders.CONTENT_LENGTH, String.valueOf(length));
-//        return ResponseEntity.ok().headers(headers).body("allGoalsPage");
-//    }
+        return "allGoalsPage";
+    }
+
 
     @PostMapping("/addGoal")
     public String addGoal(@ModelAttribute("goalDTO")GoalDTO goalDTO){
@@ -59,11 +42,6 @@ public class GoalsController extends BaseController{
         userService.addGoal(currentUserName,goalDTO);
 
         return "redirect:/allGoalsPage";
-    }
-
-    public Long goalLength(AllGoalsInfoDTO allGoalsInfoDTO){
-        String json = gson.toJson(allGoalsInfoDTO.getAllGoals());
-        return (long) json.length();
     }
 }
 
