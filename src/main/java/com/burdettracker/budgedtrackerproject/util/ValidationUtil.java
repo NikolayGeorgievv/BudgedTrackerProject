7109b297029ad591278c98d8ddc9434e55452e3a
NikolayGeorgievv <skullronx@gmail.com>
2024-05-21 23:41:57 +0300

    Implemented downloading a CSV file for transaction history.

--- src/main/java/com/burdettracker/budgedtrackerproject/util/ValidationUtil.java
+++
@@ -1,7 +0,0 @@
-package com.burdettracker.budgedtrackerproject.util;
-
-
-public interface ValidationUtil {
-
-    <E> boolean isValid(E entity);
-}
--- src/main/java/com/burdettracker/budgedtrackerproject/util/XmlParser.java
+++
@@ -1,8 +0,0 @@
-package com.burdettracker.budgedtrackerproject.util;
-
-import javax.xml.bind.JAXBException;
-
-public interface XmlParser {
-
-    <T> T fromFile(String filePath, Class<T> tClass) throws JAXBException;
-}
