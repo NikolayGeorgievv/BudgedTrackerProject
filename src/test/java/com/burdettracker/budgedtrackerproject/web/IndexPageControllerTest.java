dfdd7c50559115cd483f4b219cd246aa028efdb8
NikolayGeorgievv <skullronx@gmail.com>
2024-06-03 15:58:13 +0300

    Tests for Index page controller.

---
+++ src/test/java/com/burdettracker/budgedtrackerproject/web/IndexPageControllerTest.java
@@ -0,0 +1,45 @@
+package com.burdettracker.budgedtrackerproject.web;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class IndexPageControllerTest {
+
+    IndexPageController indexPageController;
+
+    @BeforeEach
+    void setUp() {
+        indexPageController = new IndexPageController();
+    }
+
+    @Test
+    void homePage() {
+        String viewName = indexPageController.homePage();
+        Assertions.assertEquals("index", viewName);
+    }
+
+    @Test
+    void home() {
+        String viewName = indexPageController.home();
+        Assertions.assertEquals("index", viewName);
+    }
+
+    @Test
+    void getFAQPage() {
+        String viewName = indexPageController.getFAQPage();
+        Assertions.assertEquals("/FAQsPage", viewName);
+    }
+
+    @Test
+    void getContactPage() {
+        String viewName = indexPageController.getContactPage();
+        Assertions.assertEquals("/contactsPage", viewName);
+    }
+}
