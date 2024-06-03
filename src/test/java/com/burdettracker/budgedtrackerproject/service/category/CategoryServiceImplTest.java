c2461a990ee0e33d817f1f187ceb1ec965d98378
NikolayGeorgievv <skullronx@gmail.com>
2024-06-03 14:19:19 +0300

    Get All categories test.

---
+++ src/test/java/com/burdettracker/budgedtrackerproject/service/category/CategoryServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.burdettracker.budgedtrackerproject.service.category;
+
+import com.burdettracker.budgedtrackerproject.model.entity.Category;
+import com.burdettracker.budgedtrackerproject.repository.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+class CategoryServiceImplTest {
+
+    CategoryServiceImpl categoryService;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        categoryService = new CategoryServiceImpl(categoryRepository);
+    }
+
+    @Test
+    void addCategory() {
+    }
+
+    @Test
+    void getAllCategories() {
+
+        Category category = new Category();
+        List<Category> categoriesData = new ArrayList<>();
+        categoriesData.add(category);
+
+        when(categoryRepository.findAll()).thenReturn(categoriesData);
+
+        List<Category> categories = categoryService.getAllCategories();
+
+        Assertions.assertEquals(categories.size(), 1);
+        verify(categoryRepository, times(1)).findAll();
+    }
+}
