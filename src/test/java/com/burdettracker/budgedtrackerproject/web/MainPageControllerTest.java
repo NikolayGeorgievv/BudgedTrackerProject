e305c1de24e83b994d0b5c60788073cba455dd3b
NikolayGeorgievv <skullronx@gmail.com>
2024-06-03 18:01:43 +0300

    Test for user changing his plan.

--- src/test/java/com/burdettracker/budgedtrackerproject/web/MainPageControllerTest.java
+++ src/test/java/com/burdettracker/budgedtrackerproject/web/MainPageControllerTest.java
@@ -1,5 +1,6 @@
 package com.burdettracker.budgedtrackerproject.web;
 
+import com.burdettracker.budgedtrackerproject.model.dto.membership.ChangeMembershipDTO;
 import com.burdettracker.budgedtrackerproject.model.entity.User;
 import com.burdettracker.budgedtrackerproject.model.entity.UserRoleEntity;
 import com.burdettracker.budgedtrackerproject.model.entity.enums.MembershipType;
@@ -11,19 +12,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 @SpringBootTest
@@ -41,11 +43,14 @@ class MainPageControllerTest {
     private UserServiceImpl userService;
     @Mock
     private Model model;
+    @Mock
+    private BindingResult bindingResult;
 
 
     User user;
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         user = createDummyUser();
         userRepository.saveAndFlush(user);
     }
@@ -58,14 +63,34 @@ class MainPageControllerTest {
 
     @Test
     void changeMembershipPlan() {
+        ChangeMembershipDTO changeMembershipDTO = new ChangeMembershipDTO();
+        changeMembershipDTO.setAccountToUse("MyAccount");
+        changeMembershipDTO.setMembership("FREE");
+        String userEmail = user.getEmail();
+        User user = userRepository.getByEmail(userEmail);
+        doAnswer(invocation ->{
+
+
+            user.setAccountNameAssignedForSubscription(changeMembershipDTO.getAccountToUse());
+            changeUserMembership(user, changeMembershipDTO.getMembership());
+            return null;
+        }).when(userService).changeUserPlan(changeMembershipDTO, userEmail);
+
+        mainPageController.changeMembershipPlan(changeMembershipDTO, bindingResult);
+        User updatedUser = userRepository.getByEmail(user.getEmail());
+
+
+
+        Assertions.assertEquals("FREE", updatedUser.getMembershipType().toString());
     }
 
     @Test
     void getUserByEmail() {
+        when(userService.getUserByEmail("myEmail@example.com")).thenReturn(user);
     }
 
 
-    private User createDummyUser(){
+    private User createDummyUser() {
         UUID uuid = new UUID(5, 10);
         UserRoleEntity userRoleEntity = new UserRoleEntity();
         userRoleEntity.setRole(UserRoleEnum.ADMIN);
@@ -88,5 +113,20 @@ class MainPageControllerTest {
 
         return user;
     }
-
+    private void changeUserMembership(User user, String membership) {
+        switch (membership) {
+            case "FREE":
+                user.setMembershipType(MembershipType.FREE);
+                user.setUserAccountsAllowed(1);
+                break;
+            case "GOLD":
+                user.setMembershipType(MembershipType.GOLD);
+                user.setUserAccountsAllowed(2);
+                break;
+            case "PREMIUM":
+                user.setMembershipType(MembershipType.PREMIUM);
+                user.setUserAccountsAllowed(10);
+                break;
+        }
+    }
 }
