41ab3906ced695e10340d1039df9d13a199fe4e6
NikolayGeorgievv <skullronx@gmail.com>
2024-07-10 16:25:29 +0300

    Creating a log for users log-in activity.

---
+++ src/main/java/com/burdettracker/budgedtrackerproject/aop/UserLoginAspect.java
@@ -0,0 +1,25 @@
+package com.burdettracker.budgedtrackerproject.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class UserLoginAspect {
+    private static final Logger logger = LoggerFactory.getLogger(UserLoginAspect.class);
+
+    @Pointcut("execution(* org.springframework.security.authentication.AuthenticationManager.authenticate(..))")
+    public void userLogin() {}
+
+    @AfterReturning(pointcut = "userLogin()", returning = "result")
+    public void logUserLogin(JoinPoint joinPoint, Object result) {
+        Authentication auth = (Authentication) result;
+        logger.info("User {} logged in", auth.getName());
+    }
+}
