23c4aca9d8786aebc4db2bb660a1caa28a788cd4
NikolayGeorgievv <skullronx@gmail.com>
2024-07-25 17:16:28 +0300

    Formatting.

--- src/main/java/com/burdettracker/budgedtrackerproject/BudgedTrackerProjectApplication.java
+++ src/main/java/com/burdettracker/budgedtrackerproject/BudgedTrackerProjectApplication.java
@@ -1,7 +1,6 @@
 package com.burdettracker.budgedtrackerproject;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
