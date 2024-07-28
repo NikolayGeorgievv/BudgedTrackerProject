4853e5385e62c8a813b8ca5b89f96be790b6e8ad
NikolayGeorgievv <skullronx@gmail.com>
2024-07-28 12:20:18 +0300

    Error handling.

--- src/main/java/com/burdettracker/budgedtrackerproject/exeption/GlobalExceptionHandler.java
+++ src/main/java/com/burdettracker/budgedtrackerproject/exeption/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.burdettracker.budgedtrackerproject.exeption;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,5 +14,23 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ModelAndView handleRuntimeException() {
         return new ModelAndView("error/404");
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public ModelAndView handleAccessDeniedException() {
+        return new ModelAndView("error/403");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ModelAndView handleIllegalArgumentException() {
+        return new ModelAndView("error/400");
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleInternalServerError() {
+        return new ModelAndView("error/500");
     }
 }
